cb1c06a8cd3ee9c81600d79da1e722b265c88b90
joaopaivax <joaobapaiva@gmail.com>
2022-04-03 19:30:11 -0300

    - Adicionado Médias Móveis - Correções Layout

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, render_template, jsonify
 import yfinance as yf
+import pandas as pd
+import sys
 
 # instantiate the Flask app.
 app = Flask(__name__)
@@ -13,6 +15,9 @@ def display_quote():
 
 	# pull the stock quote
 	quote = yf.Ticker(symbol)
+	
+	# pprint(vars(quote))
+	# app.logger.info(vars(quote))
 
 	#return the object via the HTTP Response
 	return jsonify(quote.info)
@@ -22,8 +27,8 @@ def display_quote():
 def display_history():
 	#get the query string parameters
 	symbol = request.args.get('symbol', default="AAPL")
-	period = request.args.get('period', default="1y")
-	interval = request.args.get('interval', default="1mo")
+	period = request.args.get('period', default="60d")
+	interval = request.args.get('interval', default="30m")
 
 	#pull the quote
 	quote = yf.Ticker(symbol)
@@ -31,6 +36,7 @@ def display_history():
 	hist = quote.history(period=period, interval=interval)
 	#convert the historical data to JSON
 	data = hist.to_json()
+	
 	#return the JSON in the HTTP response
 	return data
 
--- static/scripts/site.js
+++ static/scripts/site.js
@@ -1,5 +1,5 @@
 var Site = function(){
-	this.symbol = "MSFT";
+	this.symbol = "PETR4.SA";
 };
 
 Site.prototype.Init = function(){
@@ -8,6 +8,34 @@ Site.prototype.Init = function(){
 		$(this).val("");
 	});
 };
+
+Site.prototype.simpleMovingAverage = function (data, window = 5, last = false) {
+
+	// console.log(Object.keys(prices));
+
+	var date = Object.keys(data);
+	var prices = Object.values(data);
+
+	if (!prices || prices.length < window) {
+	  return [];
+	}
+  
+	let index = window - 1;
+	const length = prices.length + 1;
+  
+	const simpleMovingAverages = [];
+  
+	while (++index < length) {
+	  const windowSlice = prices.slice(index - window, index);
+	  const sum = windowSlice.reduce((prev, curr) => prev + curr, 0);
+	  simpleMovingAverages.push(sum / window);
+	}
+
+	if(last == true)
+		return simpleMovingAverages.slice(-1);
+
+	return simpleMovingAverages;
+}
 
 Site.prototype.GetQuote = function(){
 	// store the site context.
@@ -48,72 +76,134 @@ Site.prototype.LoadChart = function(quote){
 		method: "GET",
 		cache: false
 	}).done(function(data) {
+		console.log('Média Movel Simples (10)', that.simpleMovingAverage(JSON.parse(data).Close, 10, true));
+		console.log('Média Movel Simples (20)', that.simpleMovingAverage(JSON.parse(data).Close, 20, true));
+		console.log('Média Movel Simples (30)', that.simpleMovingAverage(JSON.parse(data).Close, 30, true));
+		console.log('Média Movel Simples (40)', that.simpleMovingAverage(JSON.parse(data).Close, 40, true));
+		console.log('Média Movel Simples (50)', that.simpleMovingAverage(JSON.parse(data).Close, 50, true));
+		console.log('Média Movel Simples (100)', that.simpleMovingAverage(JSON.parse(data).Close, 100, true));
+		console.log('Média Movel Simples (200)', that.simpleMovingAverage(JSON.parse(data).Close, 200, true));
 		that.RenderChart(JSON.parse(data), quote);
 	});
 };
 
 Site.prototype.RenderChart = function(data, quote){
 	var priceData = [];
-	var dates = [];
+	var volumeData = [];
 
 	var title = quote.shortName  + " (" + quote.symbol + ") - " + numeral(quote.price).format('$0,0.00');
 
+
+	// Close and Volume data
 	for(var i in data.Close){
-		var dt = i.slice(0,i.length-3);
-		var dateString = moment.unix(dt).format("MM/YY");
+		var volume = data.Volume[i];
 		var close = data.Close[i];
+
+		var dt = parseInt(i);
+
 		if(close != null){
-			priceData.push(data.Close[i]);
-			dates.push(dateString);
+			priceData.push([dt, close]);
 		}
+
+		if(volume != null){
+			volumeData.push([dt, volume])
+		}
+
 	}
 
-	Highcharts.chart('chart_container', {
+	console.log(volumeData);
+
+	Highcharts.stockChart('chart_container', {
+		
+		yAxis: [{
+            labels: {
+                align: 'left'
+            },
+            height: '80%',
+            resize: {
+                enabled: true
+            }
+        }, {
+            labels: {
+                align: 'left'
+            },
+            top: '80%',
+            height: '20%',
+            offset: 0
+        }],
+
+		rangeSelector: {
+			selected: 1
+		},
+	  
 		title: {
 			text: title
 		},
-		yAxis: {
-			title: {
-				text: ''
-			}
-		},
-		xAxis: {
-			categories :dates,
-		},
-		legend: {
-			layout: 'vertical',
-			align: 'right',
-			verticalAlign: 'middle'
-		},
-		plotOptions: {
-			series: {
-				label: {
-					connectorAllowed: false
-				}
-			},
-			area: {
-			}
-		},
+	  
 		series: [{
-			type: 'area',
-			color: '#85bb65',
-			name: 'Price',
-			data: priceData
+			name: quote.shortName,
+			data: priceData,
+			tooltip: {
+			  valueDecimals: 2
+			}
+		},{
+			type: 'column',
+			id: quote.shortName + '-volume',
+			name: quote.shortName + ' Volume',
+			data: volumeData,
+			yAxis: 1
 		}],
+
+		rangeSelector: {
+            buttons: [{
+                type: 'day',
+                count: 1,
+                text: '1d'
+            },{
+                type: 'day',
+                count: 3,
+                text: '3d'
+            },{
+                type: 'day',
+                count: 7,
+                text: '7d'
+            },{
+                type: 'day',
+                count: 15,
+                text: '15d'
+            },{
+                type: 'day',
+                count: 30,
+                text: '30d'
+            },{
+                type: 'month',
+                count: 3,
+                text: '3m'
+            }, {
+                type: 'day',
+                count: 7,
+                text: '7D'
+            }, {
+                type: 'all',
+                count: 1,
+                text: 'Tudo'
+            }],
+            selected: 1,
+            inputEnabled: false
+        },
+		
 		responsive: {
-			rules: [{
-				condition: {
-					maxWidth: 640
-				},
-				chartOptions: {
-					legend: {
-						layout: 'horizontal',
-						align: 'center',
-						verticalAlign: 'bottom'
-					}
-				}
-			}]
-		}
+            rules: [{
+                condition: {
+                    maxWidth: 800
+                },
+                chartOptions: {
+                    rangeSelector: {
+                        inputEnabled: false
+                    }
+                }
+            }]
+        }
 
 	});
 
@@ -123,4 +213,5 @@ var site = new Site();
 
 $(document).ready(()=>{
 	site.Init();
+	site.GetQuote();
 })
